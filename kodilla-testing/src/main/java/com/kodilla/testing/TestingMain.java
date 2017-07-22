ee9a48b1ae36be56b69b15afca445ee7c3a1586e
mrmarczyk <mrmarczyk@gmail.com>
2017-07-22 19:45:51 +0200

    Zadanie 6.2: Pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,6 +1,8 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 
 public class TestingMain {
 
@@ -16,6 +18,28 @@ public class TestingMain {
             System.out.println("test OK");
         } else {
             System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        //Testing add() method
+
+        int addResult = calculator.add(3, 1);
+
+        if (addResult == 4) {
+            System.out.println("Calculator adding test is OK");
+        } else {
+            System.out.println("Calculator has error! Result of 'Add' operation is wrong");
+        }
+
+        //Testing subtract() method
+
+        int subResult = calculator.subtract(2, 2);
+
+        if (subResult == 0) {
+            System.out.println("Calculator subtracting test is OK");
+        } else {
+            System.out.println("Calculator has error! Result of 'Sub' operation is wrong");
         }
     }
 }
