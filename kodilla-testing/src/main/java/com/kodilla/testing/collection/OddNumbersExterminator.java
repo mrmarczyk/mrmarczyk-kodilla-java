77df312494c631d40e4d4d6e0f3fae47b8187db2
mrmarczyk <mrmarczyk@gmail.com>
2017-07-23 21:23:02 +0200

    M6.3: Zadanie Test klasy wybierającej parzyste liczby z kolekcji

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+
+public class OddNumbersExterminator {
+
+    public void exterminate(ArrayList<Integer> numbers){
+
+    }
+}
