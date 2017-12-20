5d1ef5bd4b798d0874adc3f5d390398b630de7c3
Marcin Marczyk <marcinmarczyk@macbook-air-marcin.home>
2017-12-21 00:23:04 +0100

    10.2 Zadanie: Tablica z listami zadań

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/anotherversion/CalculatorV2.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/anotherversion/CalculatorV2.java
@@ -3,9 +3,9 @@ package com.kodilla.spring.calculator.anotherversion;
 public class CalculatorV2 {
 
     public void calulatorV2 (double a, double b) {
-        Add.makeCalculation(a,b);
-        Sub.makeCalculation(a,b);
-        Mul.makeCalculation(a,b);
-        Div.makeCalculation(a,b);
+        // Add.makeCalculation(a,b);
+        // Sub.makeCalculation(a,b);
+        // Mul.makeCalculation(a,b);
+        // Div.makeCalculation(a,b);
     }
 }
