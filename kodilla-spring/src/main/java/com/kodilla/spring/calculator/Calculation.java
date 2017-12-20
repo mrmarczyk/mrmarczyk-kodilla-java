2128be7c6871815d68348bcb6eaaf68677997069
Marcin Marczyk <marcinmarczyk@macbook-air-marcin.home>
2017-12-20 21:47:59 +0100

    10.2 Zadanie: Kalkulator z wyświetlaczem

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculation.java
@@ -0,0 +1,5 @@
+package com.kodilla.spring.calculator;
+
+public interface Calculation {
+    double makeCalculation(double a, double b);
+}
---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Mul.java
@@ -0,0 +1,15 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mul implements Calculation {
+    @Autowired
+    private Display display;
+
+    public double makeCalculation(double a, double b) {
+        display.displayValue(a * b);
+        return a * b;
+    }
+}
