3ccadcd1ed7fb02bf28e7a9d7e51f601dd8c93d3
Marcin Marczyk <marcinmarczyk@macbook-air-marcin.home>
2017-12-27 16:13:30 +0100

    11.4 Zadanie: Fabryka zadań :)

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/TaskFactory.java
@@ -0,0 +1,20 @@
+package com.kodilla.patterns.factory.tasks;
+
+public final class TaskFactory {
+    public static final String SHOPPING_TASK = "SHOPPING_TASK";
+    public static final String PAINTING_TASK = "PAINTING_TASK";
+    public static final String DRIVING_TASK = "DRIVING_TASK";
+
+    public final Task makeTask(final String taskClass) {
+        switch (taskClass) {
+            case SHOPPING_TASK:
+                return new ShoppingTask("TaskOne", "Tomatos", 2.0);
+            case PAINTING_TASK:
+                return new PaintingTask("TaskTwo", "Yellow", "The big white wall in the living room" );
+            case DRIVING_TASK:
+                return new DrivingTask("TaskThree", "Tesco", "Longboard");
+            default:
+                return null;
+        }
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
@@ -0,0 +1,36 @@
+package com.kodilla.patterns.factory.tasks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TaskFactoryTestSuite {
+    @Test
+    public void testFactoryShopingTask() {
+        //Given
+        TaskFactory factory = new TaskFactory();
+        //When
+        Task shoppingTask = factory.makeTask(TaskFactory.SHOPPING_TASK);
+        //Then
+        Assert.assertEquals("TaskOne", shoppingTask.getTaskName());
+    }
+
+    @Test
+    public void testFactoryPaintingTask() {
+        //Given
+        TaskFactory factory = new TaskFactory();
+        //When
+        Task paintingTask = factory.makeTask(TaskFactory.PAINTING_TASK);
+        //Then
+        Assert.assertEquals("TaskTwo", paintingTask.getTaskName());
+    }
+
+    @Test
+    public void testFactoryDrivingTask() {
+        //Given
+        TaskFactory factory = new TaskFactory();
+        //When
+        Task drivingTask = factory.makeTask(TaskFactory.DRIVING_TASK);
+        //Then
+        Assert.assertEquals("TaskThree", drivingTask.getTaskName());
+    }
+}
